5d77821520c69079a85e8e022d3f62e7da643a2d
harshitad19 <127774935+harshitad19@users.noreply.github.com>
2024-09-18 05:35:42 +0000

    done

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 270119
+let accountEmail="harshi27@gmail.com"
+var accountPassword="123456"
+accountCity="Chandigarh"
+
+accountEmail="molu07@gmail.com"
+accountPassword="272727"
+accountCity="bangalore"
+
+console.log(accountId);
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
