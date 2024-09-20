f829713efea0e60e70437eb26b86ff1301636cee
harshitad19 <127774935+harshitad19@users.noreply.github.com>
2024-09-20 15:20:46 +0000

    datatypes done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) we are using nodejs, not browser
+
+console.log(3+3)
+
+let name="hitesh"
+let age= 18
+let isLoggedIn= false
+
+//datatypes
+//primitive
+// number - int
+//bigint
+//string-" "
+//boolean- true/false
+//null(standalone value i.e empty)
+//undefined(value is not assigned to any variable)
+//symbol- unique
+
+
+//objects
+
+console.log(typeof"molu");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); //null is an object 
+console.log(typeof undefined);
+
+
