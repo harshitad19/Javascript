144d7e8526dc14a406caf8030ba9a1db01c27f0c
harshitad19 <127774935+harshitad19@users.noreply.github.com>
2024-09-24 17:15:48 +0000

    commit 1

--- 01_basics/conv.js
+++ 01_basics/conv.js
@@ -1,31 +1,62 @@
-let score="33abs"
+// let score="33abs"
 
-console.log(typeof score);
-console.log(typeof(score));
+// // console.log(typeof score);
+// // console.log(typeof(score));
 
-let valueInNumber =Number(score) //conversion of string to number 
-console.log(typeof valueInNumber);
-console.log(valueInNumber) //output will be NaN(not a number) if user enter text like 33abc because it is not a number hence it won't convert it into number
+// let valueInNumber =Number(score) //conversion of string to number 
+// // console.log(typeof valueInNumber);
+// // console.log(valueInNumber) //output will be NaN(not a number) if user enter text like 33abc because it is not a number hence it won't convert it into number
 
-//"33"=33
-// "33abc"=NaN
-//true->1
-//false->0
+// //"33"=33
+// // "33abc"=NaN
+// //true->1
+// //false->0
 
 
-let Logged= 1
+// let Logged= 1
 
-let booleanidLoggedIn=Boolean(Logged);
-console.log(typeof(booleanidLoggedIn));
-console.log(booleanidLoggedIn);
+// let booleanidLoggedIn=Boolean(Logged);
+// // console.log(typeof(booleanidLoggedIn));
+// // console.log(booleanidLoggedIn);
 
-//1->true
-//""->false
-//"molu"->true
+// //1->true
+// //""->false
+// //"molu"->true
 
-let someNumber =555
+// let someNumber =555
 
-let stringNumber=String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber);
+// let stringNumber=String(someNumber)
+// // console.log(typeof stringNumber)
+// // console.log(stringNumber);
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);//negative value for a number
+
+// // operations--->
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2**3);
+// // console.log(2/3);
+// // console.log(2%3);
+
+
+// //concatenation of strings
+// str1="harshi"
+// str2=" molu"
+
+// str3=str1+str2
+// console.log(str3);
+
+
+// //confusion between string and numbers ------------>
+// // console.log(1+"2")
+// // console.log("1"+2);
+// // console.log("1"+2+3);
+// // console.log(1+3+"3")
+
+
+
+
 
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -3,6 +3,7 @@ let accountEmail="harshi27@gmail.com"
 var accountPassword="123456"
 accountCity="Chandigarh"
 
+// accountId =121212
 accountEmail="molu07@gmail.com"
 accountPassword="272727"
 accountCity="bangalore"
