9234e78aa9762d113662a65ddaa07ee539b2c90c
harshitad19 <127774935+harshitad19@users.noreply.github.com>
2024-10-06 17:06:19 +0000

    done

--- 01_basics/conv.js
+++ 01_basics/conv.js
@@ -4,8 +4,8 @@
 let score="33abc"
 
 
-// // console.log(typeof score);
-// // console.log(typeof(score));
+console.log(typeof score);
+console.log(typeof(score));
 
 // let valueInNumber =Number(score) //conversion of string to number 
 // // console.log(typeof valueInNumber);
